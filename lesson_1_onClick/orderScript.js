dda765c01859266b7e35c063921d8f9d948a2348
Ray <5259161@gmail.com>
2024-02-07 18:24:32 +0800

    1, 建立html, css, js 檔案。 2, 加入我要訂購、認識工廠的按鈕。 3, 加入onClick 事件，跳出Alert 訊息。

---
+++ lesson_1_onClick/orderScript.js
@@ -0,0 +1,15 @@
+document.getElementById("orderButton").addEventListener("click", function () {
+    orderInfoAlert();
+});
+
+document.getElementById("factoryInfoButton").addEventListener("click", function () {
+    factoryInfoAlert();
+});
+
+function orderInfoAlert() {
+    alert('謝謝您對我們的衣服有興趣！請致電 0987-654-321，會有專人提供您報價！');
+}
+
+function factoryInfoAlert() {
+    alert('我們工廠位於新北市，通過國際 ISO9001 認證，品質讓您放心！');
+}
